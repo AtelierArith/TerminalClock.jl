a46ccd80e9f452436f0ac9f8c54ae38e114dee03
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-14 19:56:29 +0900

    impl stopwatch

--- src/TerminalClock.jl
+++ src/TerminalClock.jl
@@ -37,10 +37,43 @@ function createclock(dt=Dates.now())
 end
 
 function displayclock()
+function stopwatch(t::Time)
+    buf = IOBuffer()
+    h = hour(t)
+    m = minute(t)
+    s = second(t)
+    ms = millisecond(t)
+    h1, h2 = n2d.(divrem(h, 10))
+    m1, m2 = n2d.(divrem(m, 10))
+    s1, s2 = n2d.(divrem(s, 10))
+    ms12, ms3_ = divrem(ms, 10)
+    ms3 = n2d.(ms3_, :small)
+    ms1, ms2 = n2d.(divrem(ms12, 10), :small)
+
+    str = hcat(m1, m2, colon, s1, s2, smallcolon, ms1, ms2, ms3).str
+    print(buf, str)
+    return buf |> take! |> String
+end
+
+function stopwatch(duration=0.1)
+    start = Dates.now()
+    sleep(0.001) # warmup
     while true
         try
-            str = createclock()
+            Δ = Dates.now() - start # millisecond
+            periods = Dates.canonicalize(Dates.CompoundPeriod(Dates.Millisecond(Δ))).periods
+            str = stopwatch(Time(periods...))
             println(str)
+            sleep(duration)
+            H = length(split(str, "\n"))
+            clean(H)
+
+        catch e
+            isa(e, InterruptException) || error(e)
+            break
+        end
+    end
+end
             sleep(0.5)
             H = length(split(str, "\n"))
             clean(H)
