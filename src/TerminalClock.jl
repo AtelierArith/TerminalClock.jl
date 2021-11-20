90665ad9e23c501a4a0fbbc62da959da4e8efb55
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-21 00:51:37 +0900

    del sz identifier

--- src/TerminalClock.jl
+++ src/TerminalClock.jl
@@ -23,8 +23,8 @@ COLON_LARGE = colon(DIALS_LARGE)
 COLON_SMALL = colon(DIALS_SMALL)
 
 n2d(n::Int, s::Symbol) = n2d(n, Val(s)) # symbol dispatcher
-n2d(n::Int, sz::Val{:large}) = n2d(DIALS_LARGE, n)
-n2d(n::Int, sz::Val{:small}) = n2d(DIALS_SMALL, n)
+n2d(n::Int, ::Val{:large}) = n2d(DIALS_LARGE, n)
+n2d(n::Int, ::Val{:small}) = n2d(DIALS_SMALL, n)
 n2d(n::Int) = n2d(n, :large)
 
 function clearline(; move_up::Bool = false)
