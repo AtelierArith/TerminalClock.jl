6b488133385ba03c73ee0e0ccaeaf1e519411bcc
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-11-21 00:37:35 +0900

    rename environment variable

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,11 @@
 prefsfile = joinpath(first(DEPOT_PATH), "prefs", "TerminalClock")
 mkpath(dirname(prefsfile))
 
-toml = get(ENV, "JULIA_TERMINALCLOCK_PREF", "")
-if !isfile(toml)
+toml = get(ENV, "TERMINAL_CLOCK_TOML", "")
+if isempty(toml)
     toml = joinpath(@__DIR__, "../src/", "dials/ASCII.toml")
+elseif !isabspath(toml)
+    error("""Please set using abspath: ENV[\"TERMINAL_CLOCK_TOML"]=abspath("$toml")""")
 end
 
 # taken from IJulia/deps/build.jl
@@ -14,7 +16,7 @@ function write_if_changed(filename, contents)
 end
 
 deps = """
-const JULIA_TERMINALCLOCK_PREF=$(repr(toml))
+const TERMINAL_CLOCK_TOML=$(repr(toml))
 """
 
 write_if_changed("deps.jl", deps)
