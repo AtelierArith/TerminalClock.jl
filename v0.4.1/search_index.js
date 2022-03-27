bc2fc67fa00ef6646a045ff249d9c089025b5f98
Documenter.jl <documenter@juliadocs.github.io>
2022-03-27 11:34:11 +0000

    build based on 2b36597

---
+++ v0.4.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TerminalClock","category":"page"},{"location":"#TerminalClock","page":"Home","title":"TerminalClock","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TerminalClock.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TerminalClock]","category":"page"}]
+}
